220441cc71279487ffb9ccea92f84af432fc4a0f
Hieu Bach Van <bach_vanhieu@ca-adv.co.jp>
2023-10-31 16:52:24 +0700

    update

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,26 +1,16 @@
-import Cards, { Card } from "@/app/ui/dashboard/cards";
+import Cards from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
-import {
-  fetchCardData,
-  fetchLatestInvoices,
-  fetchRevenue,
-} from "@/app/lib/data";
+
 import {
   CardsSkeleton,
   LatestInvoicesSkeleton,
   RevenueChartSkeleton,
-} from "../ui/skeletons";
+} from "@/app/ui/skeletons";
 import { Suspense } from "react";
 
 export default async function Page() {
-  const {
-    numberOfInvoices,
-    numberOfCustomers,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
